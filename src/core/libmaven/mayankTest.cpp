e543d1fb4af1b5d2b40d5c66c782cb7e57c6ad96
mayankmtg <mayank15056@iiitd.ac.in>
2018-06-25 12:41:39 +0530

    Eigen library function testing

--- src/core/libmaven/mayankTest.cpp
+++ src/core/libmaven/mayankTest.cpp
@@ -4,37 +4,175 @@ using namespace std;
 using namespace Eigen;
 
 
-map<pair<int,int>,float> sparseRepresentation(float** a,int row, int col){
-    map<pair<int,int>,float> sparseMatrix;
+void printSparse(SparseMatrix<double> mat){
+    cerr << "printSparse"<< endl;
+    cerr << mat<< endl;
+}
+
+// 
+SparseMatrix<double> sparseRepresentation(float** a,int row, int col){
+    cerr << "sparseRepresentation" << endl;
+    SparseMatrix<double> mat(row,col);
     for(int i=0;i<row;i++){
         for(int j=0;j<col;j++){
             if(a[i][j]!=0){
-                sparseMatrix.insert(make_pair(pair<int,int>(i,j),a[i][j]));
+                mat.coeffRef(i,j)=a[i][j];
             }
         }
     }
-    return sparseMatrix;
+    return mat;
 }
 
-SparseMatrix<double> getSparseFromHash(map<pair<int,int>,float> hSparse, int n, int m){
-    SparseMatrix<double> matA(n,m);
-    map<pair<int,int>,float>::iterator itr;
-    for(itr=hSparse.begin();itr!=hSparse.end();itr++){
-        matA.coeffRef(itr->first.first, itr->first.second)=itr->second;
+SparseMatrix<double> zeroDiagSparse(float a[], int n){
+    cerr << "zeroDiagSparse" << endl;
+    SparseMatrix<double> mat(n,n);
+    for (int i=0;i<n;i++){
+        mat.coeffRef(i,i)=a[i];
     }
-    // cerr << matA << endl;
-    return matA;
+    return mat;
 }
-void booleanUpdate(float w[],float intensity[], float baseline[],int L, float p){
-    for(int i=0;i<L;i++){
-        if(intensity[i]>baseline[i]){
-            w[i]=p;
-        }
-        else if(intensity[i]<baseline[i]){
-            w[i]=1-p;
+
+void discreteMatrixDifference(float** a,int row, int column, int n){
+    cerr << "discreteMatrixDifference" << endl;
+    if(n==0){
+        return;
+    }
+    // TODO: check for the number of columns if column < n
+    // float returnMat[row][column-1];
+    for(int i=0;i<row;i++){
+        for(int j=0;j<column-1;j++){
+            a[i][j]=a[i][j+1]-a[i][j];
         }
     }
+    return discreteMatrixDifference(a, row, column-1, n-1);
 }
+
+SparseMatrix<double> matrixTranspose(SparseMatrix<double> D){
+    cerr << "matrixTranspose" << endl;
+    // SparseMatrix<double> Dt;
+    // SparseMatrix<double>::iterator itr;
+    // for(itr=D.begin();itr!=D.end();itr++){
+    //     Dt.insert(make_pair(pair<int,int>(itr->first.second,itr->first.first),itr->second));
+    // }
+    // return Dt;
+    return D.transpose();
+}
+
+// void printHashSparse(map<pair<int,int>,float> hSparse){
+//     cerr << "printHashSparse" << endl;
+//     map<pair<int,int>,float>::iterator itr;
+//     for(itr=hSparse.begin();itr!=hSparse.end();itr++){
+//         cerr << itr->first.first<<","<<itr->first.second << " " << itr->second<< endl;
+//     }
+// }
+
+// Matrix square D*D.T
+// Complexity O(mn*log(k!))
+SparseMatrix<double> matrixSquare(SparseMatrix<double> mat1, SparseMatrix<double> mat2){
+    cerr << "matrixSquare" << endl;
+    // cout << "mat1"<< endl;
+    // printHashSparse(hashMat1);
+    // cout << "mat2"<< endl;
+    // printHashSparse(hashMat2);
+    // cout << "Prod"<< endl;
+    // map<pair<int,int>,float> hashRes;
+    // pair<int,int> x;
+    // map<pair<int,int>,float>::iterator itr1,itr2,itr;
+    // for(itr1=hashMat1.begin();itr1!=hashMat1.end();itr1++){
+    //     for(itr2=hashMat2.begin();itr2!=hashMat2.end();itr2++){
+    //         if(itr1->first.second==itr2->first.first){
+    //             x=make_pair(itr1->first.first, itr2->first.second);
+    //             itr=hashRes.find(x);
+    //             if(itr==hashRes.end()){
+    //                 hashRes.insert(make_pair(x,(float)itr1->second*itr2->second));
+    //             }
+    //             else{
+    //                 itr->second+=(float)itr1->second*itr2->second;
+    //             }
+    //         }
+    //     }
+    // }
+    // return hashRes;
+
+    return mat1*mat2;
+}
+
+SparseMatrix<double> matrixSum(SparseMatrix<double> mat1, SparseMatrix<double> mat2){
+    cerr << "matrixSum" << endl;
+    // map<pair<int,int>,float>::iterator itr1, itr2, itr;
+    // map<pair<int,int>,float> finalSum;
+    // for(itr1=mat1.begin();itr1!=mat1.end();itr1++){
+    //     finalSum.insert(make_pair(pair<int,int>(itr1->first.first,itr1->first.second),itr1->second));
+    // }
+    // for(itr2=mat2.begin();itr2!=mat2.end();itr2++){
+    //     itr=finalSum.find(make_pair(itr2->first.first, itr2->first.second));
+    //     if(itr==finalSum.end()){
+    //         finalSum.insert(make_pair(pair<int,int>(itr2->first.first,itr2->first.second),itr2->second));
+    //     }
+    //     else{
+    //         itr->second+=itr2->second;
+    //     }
+    // }
+    // return finalSum;
+    return mat1+mat2;
+}
+
+SparseMatrix<double> scalarMultiplication(float scalar, SparseMatrix<double> mat){
+    cerr << "scalarMultiplication" << endl;
+    // map<pair<int,int>,float>::iterator itr;
+    // map<pair<int,int>,float> matc=mat;
+    // for(itr=matc.begin();itr!=matc.end();itr++){
+    //     itr->second*=scalar;
+    // }
+    // return matc;
+    return mat*scalar;
+}
+
+void linearMulti(float a1[], vector<float> a2, float ans[], int n){
+    cerr << "linearMulti" << endl;
+    for(int i=0;i<n;i++){
+        ans[i]=a1[i]*a2[i];
+    }
+}
+
+// SparseMatrix<double> getSparseFromHash(map<pair<int,int>,float> hSparse, int n, int m){
+//     cerr << "getSparseFromHash" << endl;
+//     SparseMatrix<double> matA(n,m);
+//     map<pair<int,int>,float>::iterator itr;
+//     for(itr=hSparse.begin();itr!=hSparse.end();itr++){
+//         matA.coeffRef(itr->first.first, itr->first.second)=itr->second;
+//     }
+//     // cerr << matA << endl;
+//     return matA;
+// }
+vector<float> solveLinearSystem(SparseMatrix<double> hermitianMat, VectorXd b){
+    cerr << "solveLinearSystem" << endl;
+    ConjugateGradient<SparseMatrix<double>, Eigen::Upper> solver;
+    solver.compute(hermitianMat);
+    VectorXd x=solver.solve(b);
+    vector<float> retVec;
+    for(int i=0;i<x.size();i++){
+        retVec.push_back((float)x[i]);
+    }
+    return retVec;
+}
+// void updateBaselineViaVector(vector<float> x){
+//     cerr << "updateBaselineViaVector" << endl;
+//     for(int i=0;i<x.size();i++){
+//         baseline[i]=x[i];
+//     }
+// }
+    // void booleanUpdate(float w[],int L, float p){
+    //     cerr << "booleanUpdate" << endl;
+    //     for(int i=0;i<L;i++){
+    //         if(intensity[i]>baseline[i]){
+    //             w[i]=p;
+    //         }
+    //         else if(intensity[i]<baseline[i]){
+    //             w[i]=1-p;
+    //         }
+    //     }
+    // }
 int main(){
     float** myTempArr;
     myTempArr=new float* [3];
@@ -50,7 +188,18 @@ int main(){
     myTempArr[2][0]=0;
     myTempArr[2][1]=0;
     myTempArr[2][2]=3;
-    SparseMatrix<double> eigenSparse = getSparseFromHash(sparseRepresentation(myTempArr, 3,3),3,3);
+    float vec[3]={1,2,3};
+    SparseMatrix<double> eigenSparse = sparseRepresentation(myTempArr, 3,3);
+    printSparse(eigenSparse);
+    printSparse(zeroDiagSparse(vec, 3));
+    printSparse(matrixTranspose(eigenSparse));
+    printSparse(matrixSquare(eigenSparse,matrixTranspose(eigenSparse)));
+    printSparse(eigenSparse+eigenSparse);
+    printSparse(scalarMultiplication(0.1,eigenSparse));
+
+
+
+    /*
     ConjugateGradient<SparseMatrix<double>, Eigen::Upper> solver;
     // HouseholderQR<SparseMatrix<double>> solver(eigenSparse);
     VectorXd b(3);
@@ -75,6 +224,6 @@ int main(){
         cout << w[i]<< " ";
     }
     cout << endl;
-
+    */
     return 0;
 }
