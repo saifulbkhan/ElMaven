0d01eacf29fc5f20b5437d66570cd064363e984e
Sahil <sahil@elucidata.io>
2018-07-02 12:51:19 +0530

    [Fix] Compilation issues with PeakClassification class

--- src/gui/mzroll/peakclassification.cpp
+++ src/gui/mzroll/peakclassification.cpp
@@ -2,60 +2,70 @@
 
 void PeakClassification::trainPeaks(vector<PeakGroup*> groups) {
 
-    map<Peak, char> allPeaks;
+    vector<Peak> allPeaks;
     for (unsigned int i = 0; i < groups.size(); i++) {
         PeakGroup* group = groups[i];
         if (!group->isMarked()) continue;
         for (unsigned int j = 0; j < group->peaks.size(); j++) {
             Peak peak = group->peaks[j];
-            allPeaks[peak] = group->label;
+            peak.label = group->label;
+            allPeaks.push_back(peak);
         }
     }
     trainPeaks(allPeaks);
 }
 
-void PeakClassification::trainPeaks(map<Peak, char> peaks) {
+void PeakClassification::trainPeaks(vector<Peak> peaks) {
 
     if (peaks.size() < 2) {
         cerr << "\nAtleast 2 classified peaks required for training model\n";
         return;
     }
 
-    vector<PeakFeatures*> features;
-    for(map<Peak, char>::iterator it= peaks.begin(); it!=peaks.end(); ++it) {
-        PeakFeatures* peakfeatures = getPeakFeatures(it->first); 
-        peakfeatures->label = it->second;
-        features.push_back(PeakFeatures);
+    vector<PeakFeatures> features;
+    for(vector<Peak>::iterator it= peaks.begin(); it!=peaks.end(); ++it) {
+        PeakFeatures peakfeatures = getPeakFeatures(*it);
+        peakfeatures.label = (*it).label;
+        features.push_back(peakfeatures);
     }
 
-    featuresToCsv(features);
+    exportFeatures(features);
 
 }
 
-PeakFeatures* PeakClassification::getPeakFeatures(Peak peak) {
+PeakFeatures PeakClassification::getPeakFeatures(Peak peak) {
 
-    PeakFeatures* peakFeatures;
-
+    PeakFeatures peakFeatures;
 	if (peak.width > 0) {
-		peakFeatures->peakAreaFractional = peak.peakAreaFractional;
-		peakFeatures->noNoiseFraction = peak.noNoiseFraction;
-		peakFeatures->symmetry = peak.symmetry / (peak.width + 1) * log2(peak.width + 1);
-		peakFeatures->groupOverlap = peak.groupOverlapFrac;
-		peakFeatures->gaussFitR2 = peak.gaussFitR2 * 100.0;
-        peakFeatures->signalToNoise = peak.signalBaselineRatio > 0 ? log2(peak.signalBaselineRatio) / 10.0 : 0;
-		peakFeatures->peakRank = peak.peakRank / 10.0;
-		peakFeatures->peakIntensity = peak.peakIntensity > 0 ? log10(peak.peakIntensity) : 0;
-		peakFeatures->skinnyPeak = peak.width <= 3 && peak.signalBaselineRatio >= 3.0 ? 1 : 0;
+		peakFeatures.peakAreaFractional = peak.peakAreaFractional;
+		peakFeatures.noNoiseFraction = peak.noNoiseFraction;
+		peakFeatures.symmetry = peak.symmetry / (peak.width + 1) * log2(peak.width + 1);
+		peakFeatures.groupOverlap = peak.groupOverlapFrac;
+		peakFeatures.gaussFitR2 = peak.gaussFitR2 * 100.0;
+        peakFeatures.signalToNoise = peak.signalBaselineRatio > 0 ? log2(peak.signalBaselineRatio) / 10.0 : 0;
+		peakFeatures.peakRank = peak.peakRank / 10.0;
+		peakFeatures.peakIntensity = peak.peakIntensity > 0 ? log10(peak.peakIntensity) : 0;
+		peakFeatures.skinnyPeak = peak.width <= 3 && peak.signalBaselineRatio >= 3.0 ? 1 : 0;
 		if (peak.peakRank / 10.0 > 1)
-			peakFeatures->peakRank = 1;
+			peakFeatures.peakRank = 1;
 	}
 	return peakFeatures;
 
 }
 
-void PeakClassification::featuresToCsv(vector<PeakFeatures*> features) {
+void PeakClassification::exportFeatures(vector<PeakFeatures> features) {
 
     QString fileName = "test.csv";
+
+    if (fileName.isEmpty()) return;
+
+    QString SEP="\t";
+    if (fileName.endsWith(".csv", Qt::CaseInsensitive)) {
+        SEP=",";
+    } else if (!fileName.endsWith(".tab", Qt::CaseInsensitive)) {
+        fileName = fileName + QObject::tr(".tab");
+        SEP="\t";
+    }
 
     QFile data(fileName);
     if (data.open(QFile::WriteOnly | QFile::Truncate)) {
@@ -71,21 +81,21 @@ void PeakClassification::featuresToCsv(vector<PeakFeatures*> features) {
 	    out << "signalToNoise" << SEP;
 	    out << "peakRank" << SEP;
 	    out << "peakIntensity" << SEP;
-	    out << "skinnyPeak" << SEP;
+	    out << "skinnyPeak" << endl;
 
 
         for(unsigned int i=0;  i < features.size(); i++ ) {
-            PeakFeatures* peakFeatures = features[i];
-            out << peakFeatures->label << SEP;
-            out << peakFeatures->peakAreaFractional << SEP;
-            out << peakFeatures->noNoiseFraction << SEP;
-            out << peakFeatures->symmetry << SEP;
-            out << peakFeatures->groupOverlap << SEP;
-            out << peakFeatures->gaussFitR2 << SEP;
-            out << peakFeatures->signalToNoise << SEP;
-            out << peakFeatures->peakRank << SEP;
-            out << peakFeatures->peakIntensity << SEP;
-            out << peakFeatures->skinnyPeak << SEP;
+            PeakFeatures peakFeatures = features[i];
+            out << peakFeatures.label << SEP;
+            out << peakFeatures.peakAreaFractional << SEP;
+            out << peakFeatures.noNoiseFraction << SEP;
+            out << peakFeatures.symmetry << SEP;
+            out << peakFeatures.groupOverlap << SEP;
+            out << peakFeatures.gaussFitR2 << SEP;
+            out << peakFeatures.signalToNoise << SEP;
+            out << peakFeatures.peakRank << SEP;
+            out << peakFeatures.peakIntensity << SEP;
+            out << peakFeatures.skinnyPeak << SEP;
             out << "\n";
         }
     }
--- src/gui/mzroll/peakclassification.h
+++ src/gui/mzroll/peakclassification.h
@@ -6,30 +6,32 @@
 #include "csvparser.h"
 #include "PeakGroup.h"
 
+struct PeakFeatures;
+
 class PeakClassification {
 
     public:
         void trainPeaks(vector<PeakGroup*> groups);
-        void trainPeaks(map<Peak, char> peaks);
+        void trainPeaks(vector<Peak> peaks);
 
     private:
-        vector<float> getPeakFeatures(Peak peak);
-        void featuresToCsv(vector<PeakFeatures*> features);
+        PeakFeatures getPeakFeatures(Peak peak);
+        void exportFeatures(vector<PeakFeatures> features);
 
 };
 
 struct PeakFeatures {
 
-    char label = '';
+    char label = '\0';
     float peakAreaFractional = 0;
-	float noNoiseFraction = 0;
-	float symmetry = 0;
-	float groupOverlap = 0;
-	float gaussFitR2 = 0;
-	float signalToNoise = 0;
-	float peakRank = 0;
-	float peakIntensity = 0;
-	float skinnyPeak = 0;
+    float noNoiseFraction = 0;
+    float symmetry = 0;
+    float groupOverlap = 0;
+    float gaussFitR2 = 0;
+    float signalToNoise = 0;
+    float peakRank = 0;
+    float peakIntensity = 0;
+    float skinnyPeak = 0;
 
 };
 
